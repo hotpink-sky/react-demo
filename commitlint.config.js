0aba4cc64d6409118abb632dbaf086f8e2d6e840
hotpink-sky <1812762000@qq.com>
2022-09-23 12:15:49 +0800

    测试提交信息检查

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,3 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
-    rules: {
-        "type-enum": [
-            // type的可选类型
-            2,
-            "always",
-            [
-                "feat",
-                "fix",
-                "docs",
-                "style",
-                "refactor",
-                "test",
-                "chore",
-                "revert",
-            ],
-        ],
-        "type-case": [2, "always", "lowerCase"], // type需小写
-        "type-empty": [2, "never"], // type不能为空
-        "subject-full-stop": [2, "never", "."], // subject不以‘.’结尾
-    },
 }
