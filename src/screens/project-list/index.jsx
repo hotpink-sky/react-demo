898179075c4a97902335bb93b394448dc1c53893
hotpink-sky <1812762000@qq.com>
2022-09-23 11:09:50 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect, React } from 'react';
+import { List } from './list';
+import { SearchPanel } from './search-panel';
+import qs from 'qs';
+import { cleanObject } from 'utils'
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+    const [users, setUsers] = useState([]);
+    const [param, setParam] = useState({
+        name: '',
+        personId: ''
+    });
+    const [list, setList] = useState([]);
+    useEffect(() => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+            if (response.ok) {
+                setList(await response.json());
+            }
+        })
+    }, [param]);
+
+    useEffect(() => {
+        fetch(`${apiUrl}/users`).then(async response => {
+            if (response.ok) {
+                setUsers(await response.json());
+            }
+        })
+    }, []);
+
+    return <div>
+        <SearchPanel param={ param } users={ users } setParam={ setParam }></SearchPanel>
+        <List list={ list } users={ users }></List>
+    </div>
+}
---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+export const isFasly = (value) => (value === 0 ? false : !value);
+export const cleanObject = (object) => {
+  // Object.assign({}, object);
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isFasly(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
