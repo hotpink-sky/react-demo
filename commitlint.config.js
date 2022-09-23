31b6208d9016fe50a815fdd765ec0f0481fc941a
hotpink-sky <1812762000@qq.com>
2022-09-23 11:30:26 +0800

    23

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-    extends: ["@commitlint/config-conventional"],
+    extends: ["@commitlint/config-angular"],
+    rules: {
+        "type-enum": [
+            // type的可选类型
+            2,
+            "always",
+            [
+                "feat",
+                "fix",
+                "docs",
+                "style",
+                "refactor",
+                "test",
+                "chore",
+                "revert",
+            ],
+        ],
+        "type-case": [2, "always", "lowerCase"], // type需小写
+        "type-empty": [2, "never"], // type不能为空
+        "subject-full-stop": [2, "never", "."], // subject不以‘.’结尾
+    },
 }
