4922016e21c43dedeb67982472299565d39db335
hotpink-sky <1812762000@qq.com>
2022-09-23 21:23:01 +0800

    feat: 抽离登录方法

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -35,7 +35,7 @@ export const LoginScreen = () => {
                 <label htmlFor="password">密码</label>
                 <input type="password" id={"password"} />
             </div>
-            <button type={"submit"}>登录</button>
+            <button type={"submit"}>注册</button>
         </form>
     )
 }
