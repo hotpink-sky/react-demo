a3deabb7d5f8d60d80d5bbf2e7164e88b93f8348
hotpink-sky <1812762000@qq.com>
2022-09-23 11:27:32 +0800

    23

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {ProjectListScreen} from 'screens/project-list';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { ProjectListScreen } from "screens/project-list"
 
 function App() {
-  return (
-    <div className="App">
-      <ProjectListScreen></ProjectListScreen> 
-    </div>
-  );
+    return (
+        <div className="App">
+            <ProjectListScreen></ProjectListScreen>
+        </div>
+    )
 }
 
-export default App;
+export default App
