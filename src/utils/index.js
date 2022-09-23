6a6e4dc878bacd55aaec273cc32f3e66d27b24cd
hotpink-sky <1812762000@qq.com>
2022-09-23 12:03:41 +0800

    测试代码检测

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,13 @@
-export const isFasly = (value) => (value === 0 ? false : !value);
-export const cleanObject = (object) => {
-  // Object.assign({}, object);
-  const result = { ...object };
-  Object.keys(result).forEach((key) => {
-    const value = result[key];
-    if (isFasly(value)) {
-      delete result[key];
-    }
-  });
-  return result;
-};
+export const isFasly = value => (value === 0 ? false : !value)
+export const cleanObject = object => {
+    // Object.assign({}, object);
+
+    const result = { ...object }
+    Object.keys(result).forEach(key => {
+        const value = result[key]
+        if (isFasly(value)) {
+            delete result[key]
+        }
+    })
+    return result
+}
